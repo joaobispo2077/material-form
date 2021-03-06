9a344e2a8842b62c7757b12f0c64486f3295eacd
João Bispo <vitor.vi10@hotmail.com>
2021-03-06 09:52:11 -0300

    refact: remade form flow

--- src/components/RegisterForm/UserData.jsx
+++ src/components/RegisterForm/UserData.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Button, TextField } from '@material-ui/core';
 
-const UserData = () => {
+const UserData = ({ handleSubmit }) => {
   return (
-    <form>
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      handleSubmit();
+    }
+    }>
       <TextField id="email" label="E-mail" type="email" variant="outlined" fullWidth required margin="normal" />
       <TextField id="password" label="Senha" type="password" variant="outlined" fullWidth required margin="normal" />
       <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import { Typography } from '@material-ui/core';
+import React, { useState } from 'react';
 import DeliveryData from './DeliveryData';
 import PersonalData from './PersonalData';
 import UserData from './UserData';
 
 function RegisterForm({ handleSubmit }) {
+  const [stage, setStage] = useState(0);
+
+  const handleNextFormStage = () => {
+    if (stage < 2) setStage(stage + 1);
+  }
+
+  const handleSelectFormStage = (stage) => {
+    switch (stage) {
+      case 0:
+        return <UserData handleSubmit={handleNextFormStage} />
+      case 1:
+        return <PersonalData handleSubmit={handleNextFormStage} />
+      case 2:
+        return <DeliveryData handleSubmit={handleSubmit} />
+      default:
+        return <Typography>Erro ao selecionar formulário</Typography>
+    }
+  }
+
   return (
-    <>
-      <PersonalData handleSubmit={handleSubmit} />
-      <UserData />
-      <DeliveryData />
-    </>
+    <>{handleSelectFormStage(stage)}</>
   )
 }
 
